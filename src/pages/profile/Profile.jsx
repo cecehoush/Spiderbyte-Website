d369969c25bfcf1a4e6e55585ab4b7655e29ebd3
Benjamin Fast <89418456+bfast3@users.noreply.github.com>
2024-11-26 15:39:52 -0700

    Fixed the tags drop down to not block the added tags (and removed scroll bars) for the overflow.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Profile.css';
 import catImage from '../../assets/cat.jpg';
+import ReactDOM from 'react-dom';
 
 function Profile({ onLogout, username, streakData, userid }) {
     const [tags, setTags] = useState([]);
@@ -134,23 +135,25 @@ function Profile({ onLogout, username, streakData, userid }) {
                     <div className="username-and-tags">
                         <h2 className="username">{username}</h2>
                         <div className="tags-container1">
-                            <button className="add-tag-button" onClick={handleAddTag}>+</button>
-                            {showTagOptions && (
-                                <div ref={dropdownRef} className="tag-dropdown">
-                                    {tags.map((tag, index) => (
-                                        <div key={index} onClick={() => handleTagSelect(tag)} className="dropdown-item">
-                                            {tag}
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                            {selectedTags.map((tag, index) => (
-                                <div key={index} className="tag1">
+                        <button className="add-tag-button" onClick={handleAddTag}>+</button>
+                        {selectedTags.map((tag, index) => (
+                            <div key={index} className="tag1">
+                                {tag}
+                                <button className="remove-tag-button" onClick={() => handleRemoveTag(tag)}>×</button>
+                            </div>
+                        ))}
+                    </div>
+
+                    {showTagOptions && (
+                        <div ref={dropdownRef} className="tag-dropdown">
+                            {tags.map((tag, index) => (
+                                <div key={index} onClick={() => handleTagSelect(tag)} className="dropdown-item">
                                     {tag}
-                                    <button className="remove-tag-button" onClick={() => handleRemoveTag(tag)}>×</button>
                                 </div>
                             ))}
                         </div>
+                    )}
+
                     </div>
                 </div>
                 <div className="streak-calendar">
